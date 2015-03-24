3bfdd98de5c4419eeabbd9dc8f33796a8fcac7a9
lmarks <logicon211@gmail.com>
2015-03-24 19:40:51 -0300

    The bird shoots balls now. To make it really stupid change the balls to be kinematic

--- TestUnityGit/Assets/Movement.cs
+++ TestUnityGit/Assets/Movement.cs
@@ -3,33 +3,30 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-	private Rigidbody2D rigidbody;
+	private Rigidbody2D playerRigidbody;
 
 	// Use this for initialization
 	void Start () {
-		rigidbody = GetComponent<Rigidbody2D>();
+		playerRigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey(KeyCode.LeftArrow)) 
 		{
-			rigidbody.velocity += (new Vector2(-transform.right.x, -transform.right.y) * 100f * Time.deltaTime);
+			playerRigidbody.velocity += (new Vector2(-transform.right.x, -transform.right.y) * 100f * Time.deltaTime);
 			//rigidbody.AddForce(-transform.right * 5000f * Time.deltaTime);
 		}
 		if(Input.GetKey(KeyCode.RightArrow)) {
-			rigidbody.AddForce(transform.right * 5000f * Time.deltaTime);
+			playerRigidbody.AddForce(transform.right * 5000f * Time.deltaTime);
 		}
 		if(Input.GetKey(KeyCode.UpArrow)) {
-			rigidbody.AddForce(transform.up * 5000f * Time.deltaTime);
+			playerRigidbody.AddForce(transform.up * 5000f * Time.deltaTime);
 		}
 
 		if(Input.GetMouseButton(0)){
 			Vector2 mousePos = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-			Debug.Log(mousePos);
-			Debug.Log(transform.position);
-			Debug.Log(new Vector2(transform.position.x - mousePos.x, transform.position.y - mousePos.y).normalized);
-			rigidbody.AddForce(new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y).normalized * 2500f * Time.deltaTime);
+			playerRigidbody.AddForce(new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y).normalized * 2500f * Time.deltaTime);
 		
 			Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		}
