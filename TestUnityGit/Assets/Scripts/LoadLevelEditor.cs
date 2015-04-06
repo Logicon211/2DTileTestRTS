8485d105586dc7988c93a1702242ea20aa264536
lmarks <logicon211@gmail.com>
2015-04-06 12:51:24 -0300

    Got unity editor code working. Can now load level from clicking the button in the editor

---
+++ TestUnityGit/Assets/Scripts/LoadLevelEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(LoadLevel))]
+public class LoadLevelEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		LoadLevel loadLevelScript = (LoadLevel)target;
+	
+		if(GUILayout.Button ("Load Level")) {
+			loadLevelScript.loadAtStartUp = false;
+			loadLevelScript.ClearLevelFromEditor();
+			loadLevelScript.InstantiateLevel();
+		}
+
+		if(GUILayout.Button ("Clear Level")) {
+			loadLevelScript.ClearLevelFromEditor();
+		}
+	}
+
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
