802a3856525184ff9deea52de728f68f749c7e0d
Logicon211 <logicon211@gmail.com>
2016-10-15 17:48:50 -0300

    Added a second ground check on the other side of player model. Also fixed getting stuck moving left and right by have the bottom of the collider be round

--- TestPlatformingAndTileMapProject/Assets/Scripts/RealMan.cs
+++ TestPlatformingAndTileMapProject/Assets/Scripts/RealMan.cs
@@ -14,18 +14,24 @@ public class RealMan : MonoBehaviour {
 	private Animator anim;
 
 
-	private Transform groundCheck;
+	private Transform groundCheck1;
+	private Transform groundCheck2;
 	private bool grounded = false;			// Whether or not the player is grounded.
 
 	// Use this for initialization
 	void Start () {
 		RB = GetComponent<Rigidbody2D>();
-		groundCheck = transform.FindChild ("groundCheck");
+		groundCheck1 = transform.FindChild ("groundCheck1");
+		groundCheck2 = transform.FindChild ("groundCheck2");
 	}
 
 	void Update() {
 		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+		bool grounded1 = Physics2D.Linecast(transform.position, groundCheck1.position, 1 << LayerMask.NameToLayer("Ground")); 
+		bool grounded2 = Physics2D.Linecast(transform.position, groundCheck2.position, 1 << LayerMask.NameToLayer("Ground"));
+		//Check in between the 2 ground checks
+		bool grounded3 = Physics2D.Linecast(groundCheck1.position, groundCheck2.position, 1 << LayerMask.NameToLayer("Ground"));
+		grounded = grounded1 || grounded2 || grounded3;
 		
 		// If the jump button is pressed and the player is grounded then the player should jump.
 		if(Input.GetAxis("Vertical") > 0 && grounded) {
